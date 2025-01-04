482a0e071bc88f0f56d5f7a6f0f843a0e834335d
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-01-04 09:07:53 +0530

    [admin] dashboard security updated

--- app/admin/components/Header.jsx
+++ app/admin/components/Header.jsx
@@ -1,8 +1,12 @@
-import { Avatar } from '@nextui-org/react'
-import { Menu } from 'lucide-react'
-import React from 'react'
+import { useAuth } from "@/context/AuthContext";
+import { useAdmin } from "@/lib/firestore/admins/read";
+import { Avatar } from "@nextui-org/react";
+import { Menu } from "lucide-react";
+import React from "react";
 
 export default function Header({ toggleSidebar }) {
+  const { user } = useAuth();
+  const { data: admin } = useAdmin({ email: user?.email });
   return (
     <section className=" flex items-center gap-3 bg-white border-b px-4 py-3">
       <div className="flex justify-center items-center md:hidden">
@@ -10,16 +14,16 @@ export default function Header({ toggleSidebar }) {
           <Menu />
         </button>
       </div>
-      <div className="w-full flex justify-between items-center pr-0 md:pr-[260px]">
+      <div className="w-full flex justify-between items-center pr-0 ">
         <h1 className="text-xl font-semibold">Dashboard</h1>
         <div className="flex gap-2 items-center">
           <div className="md:flex flex-col items-end hidden">
-            <h1 className="text-sm font-semibold">{/* {admin?.name} */}admin</h1>
-            <h1 className="text-xs text-gray-600">{/* {admin?.email} */}admin@gmail.com</h1>
+            <h1 className="text-sm font-semibold">{admin?.name}</h1>
+            <h1 className="text-xs text-gray-600">{admin?.email}</h1>
           </div>
-          <Avatar size="sm" />
+          <Avatar size="sm" src={admin?.imageURL} />
         </div>
       </div>
     </section>
-  )
+  );
 }
