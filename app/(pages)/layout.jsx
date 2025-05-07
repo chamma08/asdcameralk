1ca65375db3ccc5bcd2ff1de778945135143f286
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-05-07 10:33:52 +0530

    updated

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,9 +1,11 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Redbar from "../components/Redbar";
 
 export default function Layout({ children }) {
   return (
     <main>
+      <Redbar />
       <Header />
       {children}
       <Footer />
